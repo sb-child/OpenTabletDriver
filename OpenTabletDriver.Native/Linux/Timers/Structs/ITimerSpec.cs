023728587a016083ef1aefcfb2c8df5e269d798b
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2026-02-27 17:41:08 +0100

    Native/Linux: Convert some `struct`s to `record struct`s
    Should improve performance in comparison situations. `record struct`s
    are minimally larger in memory but has significantly reduced overhead on
    comparison operations.
    
    Also adds struct initializers on the short structs allowing for
    shorthand object construction

--- OpenTabletDriver.Native/Linux/Timers/Structs/ITimerSpec.cs
+++ OpenTabletDriver.Native/Linux/Timers/Structs/ITimerSpec.cs
@@ -3,9 +3,9 @@ using System.Runtime.InteropServices;
 namespace OpenTabletDriver.Native.Linux.Timers.Structs
 {
     [StructLayout(LayoutKind.Sequential)]
-    public struct ITimerSpec
+    public record struct ITimerSpec(TimeSpec it_interval, TimeSpec it_value)
     {
-        public TimeSpec it_interval;
-        public TimeSpec it_value;
+        public TimeSpec it_interval = it_interval;
+        public TimeSpec it_value = it_value;
     }
 }
--- OpenTabletDriver.Native/Linux/Timers/Structs/TimeSpec.cs
+++ OpenTabletDriver.Native/Linux/Timers/Structs/TimeSpec.cs
@@ -3,9 +3,9 @@ using System.Runtime.InteropServices;
 namespace OpenTabletDriver.Native.Linux.Timers.Structs
 {
     [StructLayout(LayoutKind.Sequential)]
-    public struct TimeSpec
+    public record struct TimeSpec(long sec, long nsec)
     {
-        public long sec;
-        public long nsec;
+        public long sec = sec;
+        public long nsec = nsec;
     }
 }
